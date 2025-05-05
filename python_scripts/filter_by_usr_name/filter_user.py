8d5622c984b9b534759511e36d2719151b72ad1c
Daria Chepurnaia <d89299926427@yandex.ru>
2025-05-05 10:27:02 +0300

    Filter strings by user name and write to file

---
+++ python_scripts/filter_by_usr_name/filter_user.py
@@ -0,0 +1,8 @@
+username = "alice"
+
+with open("users.txt", "r") as fin, open(f"{username}_lines.txt", "w") as fout:
+    for line in fin:
+        if username in line:
+            fout.write(line)
+
+print(f"All lines with username {username} were written to {username}_lines.txt")
